840218e09797b61db7240a13f2e2ecc42e7dd661
Laura <laumsm93@gmail.com>
2022-11-03 12:23:12 +0000

    HESTIA FINISHED

--- js/app.js
+++ js/app.js
@@ -18,14 +18,14 @@ nextBtn.addEventListener("click", () => {
         sliderPos -= 100;
         sliderContainer.style.transform = `translateX(${sliderPos}vw)`;
         prevBtn.style.display = "block";
-
+        
         if ( sliderPos === (sliderWidth - 100) * -1 ) {
             nextBtn.style.display = "none";           
         };
         
         nav.classList.add("header_nav__fixed")
     };
-
+    
 });
 
 prevBtn.addEventListener("click", () => {
@@ -33,12 +33,43 @@ prevBtn.addEventListener("click", () => {
         sliderPos += 100;
         sliderContainer.style.transform = `translateX(${sliderPos}vw)`;  
         nextBtn.style.display = "block";
-
+        
         if ( sliderPos === 0 ) {
             prevBtn.style.display = "none";
-
-            nav.classList.remove("header_nav__fixed");
             
+            nav.classList.remove("header_nav__fixed");     
+            nav.classList.remove("header_nav__fixed");
         };
     };
 });
+
+/* $(".nav-item:nth-child(1)").addEventListener("click", () => {
+    sliderPos -= 100;
+    sliderContainer.style.transform = `translateX(${-100}vw)`;
+    prevBtn.style.display = "block";
+    nav.classList.add("header_nav__fixed")
+});
+$(".nav-item:nth-child(2)").addEventListener("click", () => {
+    sliderContainer.style.transform = `translateX(${-200}vw)`;
+    sliderPos -= 200;
+    prevBtn.style.display = "block";
+    nav.classList.add("header_nav__fixed")
+});
+$(".nav-item:nth-child(3)").addEventListener("click", () => {
+    sliderContainer.style.transform = `translateX(${-300}vw)`;
+    sliderPos -= 300;
+    prevBtn.style.display = "block";
+    nav.classList.add("header_nav__fixed")
+});
+$(".nav-item:nth-child(4)").addEventListener("click", () => {
+    sliderContainer.style.transform = `translateX(${-400}vw)`;
+    sliderPos -= 400;
+    prevBtn.style.display = "block";
+    nav.classList.add("header_nav__fixed")
+});
+$(".nav-item:nth-child(5)").addEventListener("click", () => {
+    sliderContainer.style.transform = `translateX(${-500}vw)`;
+    sliderPos -= 500;
+    prevBtn.style.display = "block";
+    nav.classList.add("header_nav__fixed")
+}); */
